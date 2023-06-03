cee5600b3dc4c5a8584cf09f785a2f919b368666
Lucas Gabriel <lucas@nitro-fenix.nitro-fenix>
2023-06-03 14:35:08 -0300

    update NeuralNet

--- Classical.py
+++ Classical.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from tensorflow import keras
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score
 
-def func_learn(n:int,X,y,X_t,y_t,classifier = KNeighborsClassifier):
+
+def func_learn(n: int, X, y, X_t, y_t, classifier=KNeighborsClassifier):
     classifier = classifier()
     f1 = list()
+    acc = list()
     f1_max = 0
-    for i in range(1,n+1):
+    acc_max = 0
+    for i in range(1, n + 1):
         if type(classifier) is KNeighborsClassifier:
             classifier.n_neighbors = i
         elif type(classifier) is RandomForestClassifier:
@@ -24,44 +26,75 @@ def func_learn(n:int,X,y,X_t,y_t,classifier = KNeighborsClassifier):
             l.append(classifier.predict([j]))
         l = np.array(l)
         f1_current = f1_score(y_t, l)
+        acc_current = accuracy_score(y_t, l)
         f1_max = max(f1_max, f1_current)
+        acc_max = max(acc_max, acc_current)
         f1.append(f1_current)
-    return (f1_max)
+        acc.append(acc_current)
+    df = {'acuracia': acc, 'f1': f1}
+    df = pd.DataFrame.from_dict(df)
+    df.to_csv('resultado/' + str(classifier) + '.csv')
+    return f1_max, acc_max
 
 
-def neural_network(X,y,X_t,y_t):
-    ipt = X.shape[1]
-    l1 = (ipt+1)//2
-    l2 = (l1+1)//2
+def neural_network(X, y, X_t, y_t):
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Dense(64, activation='relu', input_shape=(1867,)),
-        tf.keras.layers.Dense(32, activation='relu'),
-        tf.keras.layers.Dense(2, activation='sigmoid')
+        tf.keras.layers.Reshape((1867, 1), input_shape=(1867,)),
+        tf.keras.layers.Conv1D(64, 3, activation='relu'),
+        tf.keras.layers.MaxPooling1D(2),
+        tf.keras.layers.Conv1D(128, 3, activation='relu'),
+        tf.keras.layers.MaxPooling1D(2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(2, activation='softmax')
     ])
 
-    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-    model.fit(X,y)
-    test_loss, test_acc = model.evaluate(X_t,y_t)
-    print(test_acc)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', tf.keras.metrics.Precision()])
+    y = tf.keras.utils.to_categorical(y, num_classes=2)
+    model.fit(X, y, epochs=15,)
+    pre = model.predict(X_t)
+    l = []
+    for i in pre:
+        l.append(np.argmax(i))
 
-
+    f1 = f1_score(y_t, l)
+    acc = accuracy_score(y_t, l)
+    return f1, acc
 
 
 def main():
-    df = pd.read_csv('data/controle_tea.dat',header=None,sep=' ')
+    df = pd.read_csv('data/controle_tea.dat', header=None, sep=' ')
     y = np.array(df[1867])
     X = np.array(df[list(range(1867))])
-    X = X.reshape([53,3,1867])
-    y = y.reshape([53,3,1])
-    x_train,x_test,y_train,y_test = train_test_split(X,y)
-    x_train = x_train.reshape([3*39, 1867])
-    x_test = x_test.reshape([3*14, 1867])
-    y_train = y_train.reshape([3*39])
-    y_test = y_test.reshape([3*14,1])
+    X = X.reshape([53, 3, 1867])
+    y = y.reshape([53, 3, 1])
 
-    # print(func_learn(30, x_train, y_train, x_test, y_test))
-    # print(func_learn(30, x_train, y_train, x_test, y_test,classifier=RandomForestClassifier))
-    neural_network(x_train, y_train, x_test, y_test)
+    f1 = [0., 0., 0.]
+    acc = [0., 0., 0.]
+
+    for i in range(10):
+        print(i)
+        x_train, x_test, y_train, y_test = train_test_split(X, y)
+        x_train = x_train.reshape([3 * 39, 1867])
+        x_test = x_test.reshape([3 * 14, 1867])
+        y_train = y_train.reshape([3 * 39])
+        y_test = y_test.reshape([3 * 14, 1])
+
+        f, a = func_learn(50, x_train, y_train, x_test, y_test)
+        f1[0] += f
+        acc[0] += a
+        f, a = func_learn(50, x_train, y_train, x_test, y_test, classifier=RandomForestClassifier)
+        f1[1] += f
+        acc[1] += a
+        f, a = neural_network(x_train, y_train, x_test, y_test)
+        f1[2] += f
+        acc[2] += a
+
+    for i in range(3):
+        f1[i] /= 10
+        acc[i] /= 10
+    print(f1)
+    print(acc)
 
 
 if __name__ == '__main__':
