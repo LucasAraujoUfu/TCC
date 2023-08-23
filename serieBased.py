6fe7e2051b1ee58d60a603730fc6c0b69362a2f3
Lucas Gabriel <lucas.teodoro@ufu.br>
2023-08-23 12:47:51 -0300

    nova gnn

--- serieBased.py
+++ serieBased.py
@@ -4,42 +4,61 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from random import shuffle
 from torch.nn import Linear
 from torch_geometric.nn import GCNConv, GATConv, GATv2Conv, TransformerConv
 from torch_geometric.nn import global_mean_pool
-from graphGen.graphGen import graphFromSeries
+from torch_geometric.loader import DataLoader
+from graphGen.tensorGraph import tGraphFromSeries
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
 
 
-class GNN(nn.Module):
-    def __init__(self, input_size, hidden_channels, conv, conv_params={}):
-        super(GNN, self).__init__()
+class GCN(nn.Module):
+    def __init__(self, num_features, hidden_channels):
+        super(GCN, self).__init__()
         # torch.manual_seed(12345)
-
-        self.conv1 = conv(
-            input_size, hidden_channels, **conv_params
-        )
-
-        self.conv2 = conv(
-            hidden_channels, hidden_channels, **conv_params
-        )
-
+        self.conv1 = GCNConv(num_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
         self.lin = Linear(hidden_channels, 2)
 
-    def forward(self, x, edge_index, batch=None, edge_col=None):
-        x = self.conv1(x, edge_index, edge_col)
+    def forward(self, x, edge_index, batch):
+        x = self.conv1(x, edge_index)
         x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
 
-        x = self.conv2(x, edge_index, edge_col)
-
-        batch = torch.zeros(x.shape[0], dtype=int) if batch is None else batch
         x = global_mean_pool(x, batch)
 
         x = F.dropout(x, p=.5, training=self.training)
         x = self.lin(x)
 
         return x
+
+
+def train(model, x):
+    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=.01)
+    criterion = nn.CrossEntropyLoss()
+    for data in x:
+        out = model(data.x, data.edge_index, data.batch)
+        loss = criterion(out, data.Y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+
+def test(model, loader):
+    model.eval()
+
+    correct = 0
+    for data in loader:
+        out = model(data.x, data.edge_index, data.batch)
+        pred = out.argmax(dim=1)
+        correct += int((pred == data.Y).sum())
+    return correct/len(loader.dataset)
 
 
 def main():
@@ -64,10 +83,9 @@ def main():
 
     for j, i in enumerate(X):
         print("gerando grafo:", j)
-        X_g.append(graphFromSeries(i, 5))
+        X_g.append(tGraphFromSeries(i, 5, [df[1867][j]]))
 
-    X_g = np.array(X_g)
-    X_g = X_g.reshape([53, 3])
+    model = GCN(1, 72813)
 
     f1 = 0.
     acc = 0.
@@ -75,82 +93,16 @@ def main():
     rcall = 0.
 
     for i in range(10):
-        print(i)
-        x_train, x_test, y_train, y_test = train_test_split(X_g, y)
-        x_train = x_train.reshape([3 * 39])
-        x_test = x_test.reshape([3 * 14])
-        y_train = y_train.reshape([3 * 39, 1])
-        y_test = y_test.reshape([3 * 14, 1])
+        # TODO: Depois que funcionar arrumar a separação do datasets (triplicatas)
+        shuffle(X_g)
+        train_dataset = DataLoader(X_g[:117], batch_size=39, shuffle=True)
+        test_dataset = DataLoader(X_g[117:], batch_size=39, shuffle=False)
 
-        x = []
-        edge_list = []
-        # edge_col = []
-        batch = []
-        for ct, j in enumerate(x_train):
-            x.append(j.vs['mag'])
-            edge_list += list(j.get_edgelist())
-            #edge_col += list(j.es['weight'])
-            batch += [ct]*len(j.get_edgelist())
-
-        x = torch.tensor(x, dtype=torch.double).to(device)
-        print(x)
-        edge_list = torch.tensor(edge_list).to(device)
-        edge_list = torch.transpose(edge_list,0,1)
-        # edge_col = torch.tensor(edge_col, dtype=torch.double).to(device)
-        # print(edge_list.size())
-        batch = torch.tensor(batch).to(device)
-
-        val_x = []
-        val_edge_list = []
-        val_batch = []
-        for ct, j in enumerate(x_test):
-            val_x += j.vs['mag']
-            val_batch += [ct] * num_features
-            val_edge_list.append(list(j.get_adjacency()))
-
-        val_x = torch.tensor(x).to(device)
-        val_x = val_x.double()
-        val_edge_list = torch.tensor(edge_list).to(device)
-        val_batch = torch.tensor(batch).to(device)
-
-        model = GNN(num_features, 500, GCNConv).to(device)
-        optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
-
-        model.train()
-        for epoch in range(20):
-            optimizer.zero_grad()
-            output = model(x, edge_list, batch)
-            loss = torch.nn.CrossEntropyLoss()(output, y_train)
-            loss.backward()
-            optimizer.step()
-
-            if epoch % 3:
-                model.eval()
-                with torch.no_grad():
-                    val_output = model(val_x, val_edge_list, val_batch)
-                    val_loss = torch.nn.CrossEntropyLoss()(val_output, y_test)
-                    print(val_output)
-                    print(val_loss)
-                model.train()
-
-
-        # TODO: fun_learn
-
-        f, a, p, r = [0, 0, 0, 0]  # fun_learn(y_test, l)
-        f1 += f
-        acc += a
-        prec += p
-        rcall += r
-
-    f1 /= 10
-    acc /= 10
-    prec /= 10
-    rcall /= 10
-
-    print(f1)
-    print(acc)
-    print(prec)
-    print(rcall)
+        for epoch in range(1, 100):
+            train(model, train_dataset)
+            train_acc = test(model, train_dataset)
+            test_acc = test(model, test_dataset)
+            print(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
 
 
 if __name__ == '__main__':
